287f01601d322cbd1bd2d38d7c014686c7d3ad85
SSE4 <tomskside@gmail.com>
2019-08-07 22:43:43 +0200

    - fix linking on linux (ffmpeg)
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -84,4 +84,9 @@ class OpenH264Conan(ConanFile):
         else:
             self.cpp_info.libs = ['openh264']
         if self.settings.os == "Linux":
-            self.cpp_info.libs.append('pthread')
+            self.cpp_info.libs.extend(['m', 'pthread'])
+        libcxx = self.settings.get_safe("compiler.libcxx")
+        if libcxx in ["libstdc++", "libstdc++11"]:
+            self.cpp_info.libs.append("stdc++")
+        elif libcxx == "libc++":
+            self.cpp_info.libs.append("c++")
