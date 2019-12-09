b6d2f9963b084db71035a8589db274443ce1849c
bincrafters-user <bincrafters@gmail.com>
2019-12-09 05:08:08 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.15.0

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class OpenH264Conan(ConanFile):
         self.build_requires("nasm/2.13.02")
         if tools.os_info.is_windows:
             if "CONAN_BASH_PATH" not in os.environ:
-                self.build_requires("msys2/20161025")
+                self.build_requires("msys2/20190524")
 
     def source(self):
         source_url = "https://github.com/cisco/openh264"
