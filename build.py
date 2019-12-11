4837e39c4c2d45ddae797c36be6e546a669bca3f
ericLemanissier <ericLemanissier@users.noreply.github.com>
2019-12-11 09:31:30 +0100

    simplify build.py

--- build.py
+++ build.py
@@ -1,26 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
-from bincrafters import build_template_default, build_shared
-
-
-def add_build_requires(builds):
-    return map(add_required_installers, builds)
-
-
-def add_required_installers(build):
-    installers = ['nasm_installer/2.13.02@bincrafters/stable']
-    if build_shared.get_os() == "Windows":
-        installers.append('msys2_installer/latest@bincrafters/stable')
-    build.build_requires.update({"*": installers})
-    return build
-
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder(pure_c=False)
-
-    builder.items = add_build_requires(builder.items)
 
     builder.run()
